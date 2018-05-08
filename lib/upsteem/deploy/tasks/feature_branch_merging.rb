d4db9bdef761f821614afb206d8bd4efbdfc2d15
Kristjan Uibo <kristjan@upsteem.com>
2018-05-08 17:40:47 +0300

    Adjust feature branch merging task and add specs

--- lib/upsteem/deploy/tasks/feature_branch_merging.rb
+++ lib/upsteem/deploy/tasks/feature_branch_merging.rb
@@ -5,8 +5,33 @@ module Upsteem
         include FeatureBranchDependent
 
         def run
-          git.create_merge_commit_from_origin(feature_branch)
+          logger.info("Starting to merge feature branch into #{environment.name} environment")
+          validate_current_branch
+          create_merge_commit
+          logger.info("Finished merging feature branch into #{environment.name} environment")
           true
+        rescue Errors::MergeConflict => e
+          handle_merge_conflict(e)
+        end
+
+        private
+
+        def branch_to_merge
+          "origin/#{feature_branch}"
+        end
+
+        def validate_current_branch
+          git.must_be_current_branch!(target_branch)
+        end
+
+        def create_merge_commit
+          git.create_merge_commit(branch_to_merge)
+        end
+
+        def handle_merge_conflict(error)
+          logger.error("Feature branch merging into #{environment.name} failed due to merge conflict")
+          git.abort_merge
+          raise error
         end
       end
     end
