b19df9b688fa776c4bb78fb3cb474144aeb86261
Kristjan Uibo <kristjan@upsteem.com>
2018-05-19 12:12:04 +0300

    Rely on the return value of system instead of last process status

--- lib/upsteem/deploy/services/system.rb
+++ lib/upsteem/deploy/services/system.rb
@@ -3,13 +3,20 @@ module Upsteem
     module Services
       class System
         def call(command)
-          system(command)
-          raise_error "No exitstatus container available for #{command.inspect}" unless $?
-          raise_error "Command #{command.inspect} failed with status #{$?.exitstatus}" unless $?.exitstatus.zero?
+          result = system(command)
+          return handle_call_failure(command, result) unless result
           true
         end
 
         private
+
+        def handle_call_failure(command, result)
+          status = $? && $?.exitstatus
+          raise_error(
+            "Command #{command.inspect} failed by returning #{result.inspect}. " \
+            "Exit status from last process: #{status.inspect}."
+          )
+        end
 
         def raise_error(message)
           raise Errors::DeployError, message
